45df042ca0d70989061ce6f477e08be4f501a755
ashutosh kumar <ashutoshk043@gmail.com>
2024-01-14 03:25:15 +0530

    added package

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 const router = require('./router/router')
 const port = 3000
 
+
+
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
